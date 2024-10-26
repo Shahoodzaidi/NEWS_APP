3cc978ce283a6af578b2c0096b54d899aa4e732f
shahood zaidi <zs83274@gmail.com>
2024-10-26 14:01:45 +0530

    add all

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/https://github.com/Shahoodzaidi/NEWS_APP.git",
   plugins: [react()],
 })
